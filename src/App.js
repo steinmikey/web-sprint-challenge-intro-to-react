f0e74c5c17a5c61667e612bf1f69800743811a46
Michael Steiner <michaelvsteiner@gmail.com>
2021-08-16 15:26:11 -0600

    Create character component and map through list in state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
+import Character from "./components/Character";
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [list, setList] = useState(null);
+  const [error, setError] = useState(null);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
@@ -15,13 +18,22 @@ const App = () => {
       .get("https://swapi.dev/api/people")
       .then((res) => {
         console.log(res.data);
+        setList(res.data);
       })
-      .catch();
-  });
+      .catch((err) => {
+        console.error(err);
+        setError("Sorry, something went wrong...");
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {error && <h2>{error}</h2>}
+      {list &&
+        list.map((ch) => {
+          return <Character key={ch.name} name={ch.name} />;
+        })}
     </div>
   );
 };
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,8 @@
 // Write your Character component here
+import React from "react";
+
+export default function Character(props) {
+  const { name } = props;
+
+  return <div>{name}</div>;
+}
